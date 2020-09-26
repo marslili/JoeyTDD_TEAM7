dd9fa4c5cd38da97a35cc8da5620a97cce0562c2
zeru <zeru@datarget.com.tw>
2020-09-26 15:46:32 +0800

    1. 非法起訖驗證

--- src/com/tdd/BudgetService.java
+++ src/com/tdd/BudgetService.java
@@ -1,4 +1,22 @@
 package com.tdd;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class BudgetService {
+    private Date startDay;
+    private Date endDay;
+
+    public BudgetService(){
+
+
+    }
+
+    public double query(LocalDate startDate, LocalDate endDate) {
+        if(startDate.isAfter(endDate))
+            return 0;
+
+
+        return -1;
+    }
 }
--- test/BudgetServerTest.java
+++ test/BudgetServerTest.java
@@ -1,2 +1,31 @@
+import com.tdd.BudgetService;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class BudgetServerTest {
+
+    BudgetService service;
+
+    @Test
+    public void test_illegalStartDay(){
+
+        service = new BudgetService();
+        LocalDate startDate;
+        startDate = LocalDate.of(2020,7,1);
+
+        LocalDate  endDate;
+        endDate = LocalDate.of(2020,6,1);
+
+
+        double resultamount = service.query(startDate,endDate);
+        Assert.assertThat(0.0,is(resultamount));
+
+
+    }
+
 }
