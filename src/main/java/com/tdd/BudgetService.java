4f9b9adcfc3681271c5a029e78a83bae936aecd6
AndyLu <andyludeveloper@gmail.com>
2020-09-27 13:54:40 +0800

    move start month and last month check in the second for loop

--- src/main/java/com/tdd/BudgetService.java
+++ src/main/java/com/tdd/BudgetService.java
@@ -24,42 +24,55 @@ public class BudgetService {
         }
 
         double totalBudget = 0.0;
-        int lengthOfEndDateMonth = YearMonth.of(endDate.getYear(), endDate.getMonth().getValue()).lengthOfMonth();
+        int lengthOfEndDateMonth = getLengthOfDateOfMonth(endDate);
         long totalMonths = getMonthBetween(startDate, endDate);
         LocalDate tmpDate;
         double amount = 0.0;
-        LocalDate startMonthFirstDate = LocalDate.of(startDate.getYear(), startDate.getMonth().getValue(), 1);
-        LocalDate endMonthLastDate = LocalDate.of(endDate.getYear(), endDate.getMonth(), lengthOfEndDateMonth);
+
+        LocalDate startMonthFirstDate = getFirstDate(startDate);
+        LocalDate endMonthLastDate = getLastDate(endDate, lengthOfEndDateMonth);
 
         for(Budget vo: budgetList){
-            for (int i = 0;i<=totalMonths;i++) {
+            for (int i = 0; i<=totalMonths; i++) {
                 tmpDate = startDate.plusMonths(i);
-                String tmpYearMon = getYearMonth(tmpDate);
-                if (vo.getYearMonth().equals(tmpYearMon)) {
+                final String yearMonth = getYearMonth(tmpDate);
+                if (vo.getYearMonth().equals(yearMonth)) {
                     amount = vo.getAmount();
+                    totalBudget += amount;
+
+                    if (tmpDate.getYear()==startDate.getYear() &&
+                            tmpDate.getMonth()==startDate.getMonth() &&
+                            startDate.getDayOfMonth() > 1) {
+                        double outOfRangeDays = DAYS.between(startMonthFirstDate, tmpDate);
+                        int lengthOfMonth = getLengthOfDateOfMonth(tmpDate);
+                        totalBudget -= (amount / lengthOfMonth) * outOfRangeDays;
+                    }
+
+                    if (tmpDate.getYear() == endDate.getYear() &&
+                            tmpDate.getMonth() == endDate.getMonth() &&
+                     endDate.getDayOfMonth() < lengthOfEndDateMonth) {
+                        double outOfRangeDays = DAYS.between(endDate, endMonthLastDate);
+                        int lengthOfMonth = lengthOfEndDateMonth;
+                        totalBudget -= (amount / lengthOfMonth) * outOfRangeDays;
+                    }
                     break;
                 }
-            }
-            totalBudget +=  amount;
-
-
-            String tmpYearMon = getYearMonth(startDate);
-            if (tmpYearMon.equals(vo.getYearMonth()) && startDate.getDayOfMonth() > 1) {
-                double outOfRangeDays = DAYS.between(startMonthFirstDate, startDate);
-                int lengthOfMonth = YearMonth.of(startDate.getYear(), startDate.getMonth().getValue()).lengthOfMonth();
-                totalBudget -= (amount / lengthOfMonth) * outOfRangeDays;
-            }
-
-
-            tmpYearMon = getYearMonth(endDate);
-            if (tmpYearMon.equals(vo.getYearMonth()) && endDate.getDayOfMonth() < lengthOfEndDateMonth) {
-                double outOfRangeDays = DAYS.between(endDate, endMonthLastDate);
-                int lengthOfMonth = lengthOfEndDateMonth;
-                totalBudget -= (amount / lengthOfMonth) * outOfRangeDays;
             }
         }
 
         return totalBudget;
+    }
+
+    private int getLengthOfDateOfMonth(LocalDate localDate) {
+        return YearMonth.of(localDate.getYear(), localDate.getMonth().getValue()).lengthOfMonth();
+    }
+
+    private LocalDate getLastDate(LocalDate endDate, int lengthOfEndDateMonth) {
+        return LocalDate.of(endDate.getYear(), endDate.getMonth(), lengthOfEndDateMonth);
+    }
+
+    private LocalDate getFirstDate(LocalDate startDate) {
+        return LocalDate.of(startDate.getYear(), startDate.getMonth().getValue(), 1);
     }
 
     private long getMonthBetween(LocalDate startDate, LocalDate endDate) {
@@ -75,16 +88,5 @@ public class BudgetService {
         }
         String yearMonth = startDate.getYear() + "" + month;
         return yearMonth;
-    }
-
-    private double getMonthBudget(String yearMonth) {
-        double amount = 0.0;
-        ArrayList<Budget> budgetList = budgetRepo.getAll();
-        for (Budget vo : budgetList) {
-            if (vo.getYearMonth().equals(yearMonth)) {
-                amount = vo.getAmount();
-            }
-        }
-        return amount;
     }
 }
